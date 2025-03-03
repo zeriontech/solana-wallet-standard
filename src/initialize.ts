0fde58d20133c1c5d9aa5d299396657adb4511f3
everdimension <everdimension@gmail.com>
2025-03-03 15:30:25 +0300

    initial commit

---
+++ src/initialize.ts
@@ -0,0 +1,9 @@
+import { registerWallet } from "./register.js";
+import { GhostWallet } from "./wallet.js";
+import type { Ghost } from "./window.js";
+
+export type { Ghost, GhostWallet };
+
+export function initialize(ghost: Ghost): void {
+  registerWallet(new GhostWallet(ghost));
+}
