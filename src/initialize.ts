528e2fec82fcfea089586d286b03ff82ab39c66a
everdimension <everdimension@gmail.com>
2025-03-03 17:00:13 +0300

    update export and add prepack script

--- src/initialize.ts
+++ src/initialize.ts
@@ -2,6 +2,7 @@ import { registerWallet } from "./register.js";
 import { GhostWallet } from "./wallet.js";
 import type { Ghost } from "./window.js";
 
+export type { WalletIcon } from "@wallet-standard/base";
 export type { Ghost, GhostWallet };
 
 export function initialize(ghost: Ghost): void {
